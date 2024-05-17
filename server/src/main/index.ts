d844de6b0208c1c6f106e1ac926090d1e96599cc
Thalles <thalles.lossurdo@invenit.com.br>
2024-05-17 10:18:46 -0300

    chore: create simple http server

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,12 +1,11 @@
 import fs from 'fs'
+import http from 'http'
 import path from 'path'
 import ytdl from 'ytdl-core'
 
-// eslint-disable-next-line import/newline-after-import
-;(async () => {
+async function downloadVideo(url: string) {
   console.time()
-  const URL = ''
-  const videoInfo = await ytdl.getInfo(URL)
+  const videoInfo = await ytdl.getInfo(url)
   console.log(videoInfo.formats)
   const format = ytdl.chooseFormat(videoInfo.formats, { quality: '18' })
   const outputFilePath = `${path.resolve(__dirname + '../../../uploads')}/${
@@ -20,4 +19,19 @@ import ytdl from 'ytdl-core'
     console.log(`Finished download: ${outputFilePath}`)
   })
   console.timeEnd()
-})()
+}
+
+http
+  .createServer(async (_, res) => {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+  })
+  .on('request', async (req, res) => {
+    const [, url] = req.url?.split('?url=') ?? []
+    // await downloadVideo(url)
+    res.end(url)
+  })
+  .listen(3000, () => {
+    console.info('Server is running!')
+  })
